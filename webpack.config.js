1db0a2d5a73751edf7be1657b5c7110df2163978
Rajanikanth0 <61695662+Rajanikanth0@users.noreply.github.com>
2025-12-17 11:00:01 +0530

    setup: add css loaders and files

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,15 @@ module.exports = {
     clean: true,
   },
 
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
