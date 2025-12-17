0077a817a56eae4a6a62dad5ceed57dba50234df
Rajanikanth0 <61695662+Rajanikanth0@users.noreply.github.com>
2025-12-17 14:03:37 +0530

    feat: create and insert all the elements using js

---
+++ src/elements.js
@@ -0,0 +1,35 @@
+function initialPageLoad() {
+  // elements to append on
+  const content = document.querySelector("#content");
+
+  // elements creation
+  
+  const heroSection = document.createElement("div");
+  heroSection.classList.add("hero-section");
+
+  const title = document.createElement("h1");
+  title.classList.add("title");
+  title.textContent = "Restaurant Page";
+
+  const description = document.createElement("p");
+  description.classList.add("description");
+  description.textContent = "Discover the best food & drinks around the corder";
+
+  const search = document.createElement("div");
+  search.classList.add("search");
+
+  const icon = document.createElement("div");
+  icon.classList.add("icon");
+
+  const input = document.createElement("input");
+  input.setAttribute("type", "search");
+  input.setAttribute("placeholder", "Search for food...");
+
+  // assign elements
+
+  search.append(icon, input);
+  heroSection.append(title, description, search);
+  content.appendChild(heroSection);
+}
+
+export { initialPageLoad };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import "./styles.css";
+import { initialPageLoad } from "./elements.js";
+
+initialPageLoad();
