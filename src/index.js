a7c83eee9f84c45fb98084bfa0572c9dd60b55c8
Rajanikanth0 <61695662+Rajanikanth0@users.noreply.github.com>
2025-12-18 14:20:01 +0530

    feat: add logic on nav buttons to render page

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import { homeContent } from "./tabs/home.js";
 import { menuContent } from "./tabs/menu.js";
 import { aboutContent } from "./tabs/about.js";
 
-// homeContent();
-// menuContent();
-aboutContent();
+// initial content
+homeContent();
+
+const header = document.querySelector("header");
+const content = document.querySelector("#content");
+
+function changeTab(e) {
+  const target = e.target.dataset;
+  if (!target) return;
+
+  // clean page content
+  content.textContent = "";
+
+  switch(target.button) {
+    case "home": homeContent(); break;
+    case "menu": menuContent(); break;
+    case "about": aboutContent(); break;
+  }
+}
+
+header.addEventListener("click", changeTab);
