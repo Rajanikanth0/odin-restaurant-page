70c36938991654440f62c41fc93d533f3efe48e7
Rajanikanth0 <61695662+Rajanikanth0@users.noreply.github.com>
2025-12-18 14:05:49 +0530

    feat: add about content using javascript

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./styles.css";
 import { homeContent } from "./tabs/home.js";
 import { menuContent } from "./tabs/menu.js";
+import { aboutContent } from "./tabs/about.js";
 
 // homeContent();
 // menuContent();
+aboutContent();
