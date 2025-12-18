ea814371c6f145fa647e965488cb687406315f2f
Rajanikanth0 <61695662+Rajanikanth0@users.noreply.github.com>
2025-12-18 10:02:04 +0530

    feat: add menu content using javascript

--- src/tabs/menu.js
+++ src/tabs/menu.js
@@ -1,5 +1,35 @@
+function getCard() {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const image = document.createElement("div");
+  image.classList.add("image");
+
+  const title = document.createElement("h2");
+  title.classList.add("title");
+  title.textContent = "Recipe Name";
+
+  const description = document.createElement("p");
+  description.classList.add("description");
+  description.textContent = "a brief sentence about the recipe";
+
+  card.append(image, title, description);
+
+  return card;
+}
+
 function menuContent() {
-  const content = document.querySelector("content");
+  const content = document.querySelector("#content");
+
+  const menu = document.createElement("div");
+  menu.classList.add("menu");
+
+  for (let n = 0; n < 10; n++) {
+    const card = getCard();
+    menu.appendChild(card);
+  }
+
+  content.appendChild(menu);
 }
 
 export { menuContent };
